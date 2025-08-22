70ebb06415a3d03c2bf5e29dd9acfef1c10ef25e
Ramakant Mishra <ramakantm921@gmail.com>
2025-08-22 13:42:04 +0530

    chapter 3

---
+++ chapter 3/1_string.py
@@ -0,0 +1,7 @@
+name = "Maximus"
+number = "374523562897483"
+
+# nameshort = name[-5 : -1]
+nameshort = number[0::5]
+
+print(nameshort)
---
+++ chapter 3/2_str_function.py
@@ -0,0 +1,5 @@
+name = "Harrison"
+
+print(len(name))
+print(name.endswith("son"))
+print(name.startswith("Har"))
