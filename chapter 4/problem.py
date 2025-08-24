be42dedcae5da0062a64894deae68344f522f9ba
Ramakant Mishra <ramakantm921@gmail.com>
2025-08-25 01:15:34 +0530

    problem

--- chapter 4/problem.py
+++ chapter 4/problem.py
@@ -1,22 +1,32 @@
 # Problem 1
 
-marks = []
+# marks = []
 
-f1 = input("Enter marks of student 1  : ")
-marks.append(f1) 
-f2 = input("Enter marks of student 2  : ")
-marks.append(f2) 
-f3 = input("Enter marks of student 3  : ")
-marks.append(f3) 
-f4 = input("Enter marks of student 4  : ")
-marks.append(f4) 
-f5 = input("Enter marks of student 5  : ")
-marks.append(f5) 
-f6 = input("Enter marks of student 6  : ")
-marks.append(f6)
+# f1 = int (input("Enter marks of student 1 out of 100 : "))
+# marks.append(f1) 
+# f2 = int (input("Enter marks of student 2 out of 100 : "))
+# marks.append(f2) 
+# f3 = int (input("Enter marks of student 3 out of 100 : "))
+# marks.append(f3) 
+# f4 = int (input("Enter marks of student 4 out of 100 : "))
+# marks.append(f4) 
+# f5 = int (input("Enter marks of student 5 out of 100 : "))
+# marks.append(f5) 
+# f6 = int (input("Enter marks of student 6 out of 100 : "))
+# marks.append(f6)
 
-print(marks)
+# print(marks)
 
 # Problem 2
 
-average
+# average = (f1+f2+f3+f4+f5+f6)//6 
+
+# print("Average: ",average)
+# marks.sort()
+# print(marks)
+
+# Problem 3
+
+l = [56,34,78,32,89,76,21,90,76,754,34,27,987,586,634,42,3231,]
+
+print(sum(l))
