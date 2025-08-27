578e5ab9578ba3665f18a4035f20b08ba582a373
Ramakant Mishra <ramakantm921@gmail.com>
2025-08-27 13:09:38 +0530

    problem

---
+++ chapter 6/problem.py
@@ -0,0 +1,57 @@
+# Problem 1
+
+# a1 = int(input("Enter number 1: "))
+# a2 = int(input("Enter number 2: "))
+# a3 = int(input("Enter number 3: "))
+# a4 = int(input("Enter number 4: "))
+
+# if(a1>a2 and a1>a3 and a1>a4):
+#     print("Greatest number is a1: ",a1)
+    
+# elif(a2>a1 and a2>a3 and a2>a4):
+#     print("Greatest number is a2: ",a2)
+    
+# elif(a3>a2 and a3>a1 and a3>a4):
+#     print("Greatest number is a3: ",a3)
+    
+# elif(a4>a2 and a4>a3 and a4>a1):
+#     print("Greatest number is a4: ",a4)
+    
+    
+
+    # Problem 2
+    
+# mark1 = int(input("enter marks 1: "))    
+# mark2 = int(input("enter marks 2: "))    
+# mark3 = int(input("enter marks 3: "))    
+ 
+# # check for total percentage
+# total_percentage = ((mark1+mark2+mark3)/300)*100
+
+# if(total_percentage >= 40 and mark1 >= 33 and mark2 >= 33 and mark3 >= 33):
+#     print("You have passed exam: ",total_percentage)
+    
+# else:
+#     print("You have failed exam: ",total_percentage)
+    
+    
+    
+    # Problem 3
+    
+c1= "make a lots of money"
+c2= "buy now"
+c3= "subscribe now"
+c4= "click this"   
+c5= "and all set"
+        
+comments = input("Enter comments: ")
+
+if((c1 in comments) or (c2 in comments) or (c3 in comments) or (c4 in comments)):
+    
+    print("It's a spam comment")
+    
+elif(c5 in comments):
+    print("It's the best word!") 
+    
+else:
+    print("It's a true comment")
