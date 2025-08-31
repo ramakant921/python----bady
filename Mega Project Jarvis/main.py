ebd1372dc7ded4d98965b4372e4eadf5b60373ad
Ramakant Mishra <ramakantm921@gmail.com>
2025-08-31 17:19:31 +0530

    jarvis

--- Mega Project Jarvis/main.py
+++ Mega Project Jarvis/main.py
@@ -8,6 +8,10 @@ engine = pyttsx3.init()
 def speak(text):
     engine.say(text)
     engine.runAndWait()
+    
+def processCommand(c):
+    print(c)
+    pass
 
 if __name__ == "__main__":
     speak("Initialiazing Jarvis.........")
@@ -16,15 +20,24 @@ if __name__ == "__main__":
         # Listen wake word from "Jarvis"
         # Obtain audio from the microphone
         r = sr.Recognizer()
-        with sr.Microphone() as source:
-            print("Listening.....")
-            audio = r.listen(source,timeout = 2, phrase_time_limit= 1)
+        
         
         
     # Recognize speech using Sphinx
-    print("recoginitizing.........")
-    try:
-        command = r.recognize_google(audio)
-        print(command)
-    except Exception as e:
-        print("Error; {0}" .format(e))
+        print("recoginitizing...")
+        try:
+            with sr.Microphone() as source:       
+                print("Listening.....")
+                audio = r.listen(source, timeout = 2, phrase_time_limit = 1)
+            word = r.recognize_google(audio)
+            if(word.lower() == 'jarvis'):
+                speak("Ya")
+                with sr.Microphone() as source:
+                    print("Jarvis active....")
+                    audio = r.listen(source)
+                    command = r.recognize_google(audio)
+                    
+                    processCommand(command)
+                                 
+        except Exception as e:
+            print("Error; {0}" .format(e))
